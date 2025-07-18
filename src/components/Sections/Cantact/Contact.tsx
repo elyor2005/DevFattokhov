d7b53d501cc80ad39edf0e781ec9cd6d3381d3e1
Elyor <elyorabdufattokhov@gmail.com>
2025-07-18 19:12:38 +0500

    fix contact

--- src/components/Sections/Cantact/Contact.tsx
+++ src/components/Sections/Cantact/Contact.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import Container from "../../Shared/container";
+
 import SectionHeader from "../../Shared/Header";
 import ContactSection from "./CantactUI";
 
 const Contact = () => {
   return (
     <section id="/contact">
-      <Container>
-        <SectionHeader title="Contact">
-          <div>
-            <ContactSection />
-          </div>
-        </SectionHeader>
-      </Container>
+      <SectionHeader title="Contact">
+        <div>
+          <ContactSection />
+        </div>
+      </SectionHeader>
     </section>
   );
 };
